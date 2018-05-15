1f577fde6a50b4b575966aeb81405cc73949f496
Rhys Angelo-Jones <rhystaj@gmail.com>
2018-05-15 20:59:45 +1200

    Create Database Test Function

--- functions/index.js
+++ functions/index.js
@@ -4,5 +4,10 @@ const functions = require('firebase-functions');
 // // https://firebase.google.com/docs/functions/write-firebase-functions
 //
 // exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+//   response.send("Hello from Firebase!");
+//  });
+
+ exports.realtimeTest = functions.database.ref('requests').onWrite((snap, cont) => {
+  console.log("hi");
+  return;
+ });
