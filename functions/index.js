dbc8d668882ed97d9b58cd35829b41aac87352dc
Rhys Angelo-Jones <rhystaj@gmail.com>
2018-05-17 12:37:23 +1200

    Still working on getting user keys.

--- functions/index.js
+++ functions/index.js
@@ -1,13 +1,37 @@
 const functions = require('firebase-functions');
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//   response.send("Hello from Firebase!");
-//  });
+var usersSnap;
 
- exports.realtimeTest = functions.database.ref('requests').onWrite((snap, cont) => {
-  console.log("hi");
-  return;
+/*
+    Fires when a user send a request to the database.
+*/
+ exports.requestRecieved = functions.database.ref('/requests/{pushId}').onCreate((snap, cont) => {
+    
+    const usersRef = snap.ref.parent.parent.child('users');
+
+    return new Promise((resolve, reject) => {
+        
+
+        const possibleHelpers = getPossibleHelpers(snap.val(), usersSnap.val());
+        possibleHelpers.array.forEach(element => {
+             usersRef.child(element).set(true);
+        });
+
+    });
+
  });
+
+ exports.changeInUsers = functions.database.ref("/users").onUpdate((change, cont) => {
+     usersSnap = change.after;
+ })
+
+/**
+ * Get a collections of users that could help with the given request.
+ * @param {The request to search for help for.} request 
+ */
+function getPossibleHelpers(request, usersRef){
+
+    users = Object.keys(usersObj);
+    return users.array.filter(user => true);
+
+}
